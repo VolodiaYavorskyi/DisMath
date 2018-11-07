66b161040b627874bfd9015d4f0f7e95dc119545
VolodiaYavorskyi <43553028+VolodiaYavorskyi@users.noreply.github.com>
2018-11-07 23:40:53 +0200

    full

--- Lab 3/dm3.c
+++ Lab 3/dm3.c
@@ -6,6 +6,7 @@ int main()
     int n = 100;
     int A[n];
     int B[n];
+    
     // matrix
     int C[n][n];
     
@@ -16,6 +17,7 @@ int main()
     } while (n < 1);
     
     // initialisation of A and B
+    
     for (int i = 0; i < n; i++)
     {
         printf("A[%d] = ", i + 1);
@@ -28,12 +30,13 @@ int main()
     }
     
     // matrix
+    
     for (int i = 0; i < n; i++)
     {
         printf("| ");
         for (int j = 0; j < n; j++)
         {
-            if(A[i] > 2*B[j])
+            if (A[i] > 2*B[j])
             {
                 C[i][j] = 1;
                 printf("%d ", C[i][j]);
@@ -49,19 +52,99 @@ int main()
     }
     
     // reflexivity
+    
+    for (int j = 0; j < n; j++)
+    {
+        if (C[j][j] != 1)
+        {
+            printf("Not reflexive");
+            break;
+        }
+        else if (j == n - 1)
+            printf("Reflexive");
+    }
+    printf("\n");
+    
+    // antireflexivity
+    
+    for (int j = 0; j < n; j++)
+    {
+        if (C[j][j] != 0)
+        {
+            printf("Not antireflexive");
+            break;
+        }
+        else if (j == n - 1)
+            printf("Antireflexive");
+    }
+    printf("\n");
+    
+    // symmetry
+    
+    for (int i = 0, k = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (C[i][j] != C[j][i] && i != j)
+            {
+                k = 1;
+                printf("Not symmetic");
+                break;
+            }
+            else if (i == n - 1 && j == n - 1)
+                printf("Symmetric");
+        }
+        if (k)
+            break;
+    }
+    printf("\n");
+    
+    // antisymmetry
+    
+    for (int i = 0, l = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (C[i][j] == C[j][i] && i != j)
+            {
+                l = 1;
+                printf("Not antisymmetic");
+                break;
+            }
+            else if (i == n - 1 && j == n - 1)
+                printf("Antisymmetric");
+        }
+        if (l)
+            break;
+    }
+    printf("\n");
+    
+    // transitivity
+    
+    bool transitive = true;
+    bool antitransitive = true;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if(C[j][j] == 1)
+            for (int k = 0; k < n; k++)
             {
-                printf("reflexive");
-            }
-            else
-            {
-                printf("not reflexive");
+                if (j == k == i)
+                    continue;
+                transitive = transitive & (!C[i][j] || C[j][k] || C[i][k]);
+                antitransitive = antitransitive & (!C[i][j] || !C[j][k] || !C[i][k]);
             }
         }
-        printf("\n");
     }
+    if (transitive)
+        printf("Transitive\n");
+    else
+        printf("Not transitive\n");
+        
+    if (antitransitive)
+        printf("Antitransitive\n");
+    else
+        printf("Not antitransitive\n");
+        
+    return 0;
 }
